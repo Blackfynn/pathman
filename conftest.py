850b70d45be62fec27e1f063440b4b8f35339c88
Ben Siderowf <ben@blackfynn.com>
2019-06-05 10:56:18 -0400

    Add integration option

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--integration", action="store_true",
+                     help="run integration tests")
+
+
+def pytest_runtest_setup(item):
+    if 'integration' in item.keywords and
+    not item.config.getoption("--integration"):
+
+        pytest.skip("Needs --integration option to run")
